c3568bf95b0b1a120275f2eb0332bdcf231a3b0d
Steven Peniche <stevenmpeniche@gmail.com>
2018-02-12 18:14:07 -0500

    Initialize 'Game Component'

---
+++ src/Components/Game/Game.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="Game" >
+        Hello Game
+      </div>
+    );
+  }
+}
+
+export default Game;
