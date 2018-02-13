31474ee779d45440a77825f5388a32252bfb2fda
Steven Peniche <stevenmpeniche@gmail.com>
2018-02-13 12:29:58 -0500

    Add 'selectedNumbers state' to 'Game' Component

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -1,19 +1,17 @@
 import React, { Component } from "react";
-import _ from 'lodash';
+import _ from "lodash";
 
-const Stars = (props) => {
-  const numberOfStars = 1 + Math.floor(Math.random()*9);
+const Stars = props => {
+  const numberOfStars = 1 + Math.floor(Math.random() * 9);
 
   return (
     <div className="col-5">
-      {_.range(numberOfStars).map(i =>
-        <i key={i} className="fas fa-star"></i>
-      )}
+      {_.range(numberOfStars).map(i => <i key={i} className="fas fa-star" />)}
     </div>
   );
-}
+};
 
-const Button = (props) => {
+const Button = props => {
   return (
     <div className="col-2">
       <button>=</button>
@@ -21,20 +19,28 @@ const Button = (props) => {
   );
 };
 
-const Answer = (props) => {
+const Answer = props => {
   return (
     <div className="col-5">
-      ...
+      {props.selectedNumbers.map((number, i) =>
+        <span key={i}>{number}</span>
+      )}
     </div>
-  );
+  )
 };
 
-const Numbers = (props) => {
+const Numbers = props => {
+  const numberClassName = (number) => {
+    if(props.selectedNumbers.indexOf(number) >= 0) {
+      return 'selected';
+    }
+  }
+
   return (
     <div className="card text-center">
       <div>
         {Numbers.list.map((number, i) =>
-          <span key={i}>{number}</span>
+          <span key={i} className={numberClassName(number)}>{number}</span>
         )}
       </div>
     </div>
@@ -43,18 +49,22 @@ const Numbers = (props) => {
 Numbers.list = _.range(1, 10);
 
 class Game extends Component {
+  state = {
+    selectedNumbers: [2, 4],
+  };
+
   render() {
     return (
-      <div className="container" >
+      <div className="container">
         <h3>Play Nine</h3>
         <hr />
         <div className="row">
           <Stars />
           <Button />
-          <Answer />
+          <Answer selectedNumbers={this.state.selectedNumbers}/>
         </div>
         <br />
-        <Numbers />
+        <Numbers selectedNumbers={this.state.selectedNumbers}/>
       </div>
     );
   }
