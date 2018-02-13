456f9947ad4444c42bdb7b318b6dc5ea4629ceb9
Steven Peniche <stevenmpeniche@gmail.com>
2018-02-13 14:11:23 -0500

    Add 'Answer Verification'

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -10,9 +10,32 @@ const Stars = props => {
 };
 
 const Button = props => {
+  let button;
+  switch(props.answerIsCorrect) {
+    case true:
+      button =
+        <button className="btn btn-success">
+          <i className="fas fa-check"></i>
+        </button>
+      break;
+    case false:
+      button =
+        <button className="btn btn-danger">
+          <i className="fas fa-times"></i>
+        </button>
+      break;
+    default:
+      button =
+      <button className="btn btn-secondary"
+              onClick={props.checkAnswer}
+              disabled={props.selectedNumbers.length === 0}>
+        =
+      </button>
+      break;
+  }
   return (
     <div className="col-2">
-      <button className="btn btn-secondary" disabled={props.selectedNumbers.length === 0}>=</button>
+      {button}
     </div>
   );
 };
@@ -53,6 +76,7 @@ class Game extends Component {
   state = {
     selectedNumbers: [],
     randomNumberOfStars: 1 + Math.floor(Math.random() * 9),
+    answerIsCorrect: null,
   };
 
   selectNumber = (clickedNumber) => {
@@ -68,15 +92,27 @@ class Game extends Component {
     }));
   };
 
+  checkAnswer = () => {
+    this.setState(prevState => ({
+      answerIsCorrect: prevState.randomNumberOfStars === prevState.selectedNumbers.reduce((acc, n) => acc + n, 0)
+    }));
+  }
   render() {
-    const { selectedNumbers, randomNumberOfStars } = this.state;
+    const {
+      selectedNumbers,
+      randomNumberOfStars,
+      answerIsCorrect
+    } = this.state;
+
     return (
       <div className="container">
         <h3>Play Nine</h3>
         <hr />
         <div className="row">
           <Stars numberOfStars={randomNumberOfStars}/>
-          <Button selectedNumbers={selectedNumbers}/>
+          <Button selectedNumbers={selectedNumbers}
+                  checkAnswer={this.checkAnswer}
+                  answerIsCorrect={answerIsCorrect}/>
           <Answer selectedNumbers={selectedNumbers} rollBackAnswer={this.rollBackAnswer}/>
         </div>
         <br />
