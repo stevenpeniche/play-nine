4c99493a2ce8153fa4228381d87dbdf8dce5ad36
Steven Peniche <stevenmpeniche@gmail.com>
2018-02-13 13:27:18 -0500

    Add 'Number Selection' Functionality

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -2,11 +2,9 @@ import React, { Component } from "react";
 import _ from "lodash";
 
 const Stars = props => {
-  const numberOfStars = 1 + Math.floor(Math.random() * 9);
-
   return (
     <div className="col-5">
-      {_.range(numberOfStars).map(i => <i key={i} className="fas fa-star" />)}
+      {_.range(props.numberOfStars).map(i => <i key={i} className="fas fa-star" ></i>)}
     </div>
   );
 };
@@ -14,7 +12,7 @@ const Stars = props => {
 const Button = props => {
   return (
     <div className="col-2">
-      <button>=</button>
+      <button className="btn btn-secondary" disabled={props.selectedNumbers.length === 0}>=</button>
     </div>
   );
 };
@@ -23,7 +21,7 @@ const Answer = props => {
   return (
     <div className="col-5">
       {props.selectedNumbers.map((number, i) =>
-        <span key={i}>{number}</span>
+        <span key={i} onClick={() => props.rollBackAnswer(number)}>{number}</span>
       )}
     </div>
   )
@@ -40,7 +38,10 @@ const Numbers = props => {
     <div className="card text-center">
       <div>
         {Numbers.list.map((number, i) =>
-          <span key={i} className={numberClassName(number)}>{number}</span>
+          <span key={i} className={numberClassName(number)}
+                onClick={() => props.selectNumber(number)}>
+            {number}
+          </span>
         )}
       </div>
     </div>
@@ -50,21 +51,36 @@ Numbers.list = _.range(1, 10);
 
 class Game extends Component {
   state = {
-    selectedNumbers: [2, 4],
+    selectedNumbers: [],
+    randomNumberOfStars: 1 + Math.floor(Math.random() * 9),
+  };
+
+  selectNumber = (clickedNumber) => {
+    if(this.state.selectedNumbers.indexOf(clickedNumber) >= 0) { return }
+    this.setState(prevState => ({
+      selectedNumbers: prevState.selectedNumbers.concat(clickedNumber)
+    }));
+  };
+
+  rollBackAnswer = (clickedNumber) => {
+    this.setState(prevState => ({
+      selectedNumbers: prevState.selectedNumbers.filter(number => number !== clickedNumber)
+    }));
   };
 
   render() {
+    const { selectedNumbers, randomNumberOfStars } = this.state;
     return (
       <div className="container">
         <h3>Play Nine</h3>
         <hr />
         <div className="row">
-          <Stars />
-          <Button />
-          <Answer selectedNumbers={this.state.selectedNumbers}/>
+          <Stars numberOfStars={randomNumberOfStars}/>
+          <Button selectedNumbers={selectedNumbers}/>
+          <Answer selectedNumbers={selectedNumbers} rollBackAnswer={this.rollBackAnswer}/>
         </div>
         <br />
-        <Numbers selectedNumbers={this.state.selectedNumbers}/>
+        <Numbers selectedNumbers={selectedNumbers} selectNumber={this.selectNumber}/>
       </div>
     );
   }
